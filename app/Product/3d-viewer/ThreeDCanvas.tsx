ce5c23a584a84c1019b5d9aa463b819e7fddc932
Mrkushal-04 <kushalminz04@gmail.com>
2025-08-04 17:03:32 +0530

    Refactor 3D model rendering and update TypeScript configuration

--- app/Product/3d-viewer/ThreeDCanvas.tsx
+++ app/Product/3d-viewer/ThreeDCanvas.tsx
@@ -11,10 +11,10 @@ function ProductModel() {
         if (meshRef.current) meshRef.current.rotation.y += 0.005;
     });
     return (
-        <mesh ref={meshRef} position={[0, 0, 0]}>
-            <boxGeometry args={[2, 3, 1]} />
-            <meshStandardMaterial color="#666666" />
-        </mesh>
+        <primitive object={new THREE.Mesh(
+            new THREE.BoxGeometry(2, 3, 1),
+            new THREE.MeshStandardMaterial({ color: "#666666" })
+        )} ref={meshRef} position={[0, 0, 0]} />
     );
 }
 
@@ -25,13 +25,13 @@ export default function ThreeDCanvas({ cameraDistance = 5, autoRotate = true, sh
             <OrbitControls autoRotate={autoRotate} />
             {showLights && (
                 <>
-                    <ambientLight intensity={0.5} />
-                    <directionalLight position={[10, 10, 5]} intensity={1} />
-                    <pointLight position={[-10, -10, -5]} intensity={0.5} />
+                    <ambientLight intensity={0.5} {...({} as any)} />
+                    <directionalLight position={[10, 10, 5]} intensity={1} {...({} as any)} />
+                    <pointLight position={[-10, -10, -5]} intensity={0.5} {...({} as any)} />
                 </>
             )}
             <ProductModel />
-            {showGrid && <gridHelper args={[10, 10]} />}
+            {showGrid && <gridHelper args={[10, 10]} {...({} as any)} />}
             <Environment preset="studio" />
         </Canvas>
     );
